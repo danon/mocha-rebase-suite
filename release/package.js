289b982e39c636a18cb3508252a24f27d469aa4b
Daniel Wilkowski <wilkowski.kontakt@gmail.com>
2024-03-08 07:41:13 +0100

    Add release/

---
+++ release/package.js
@@ -0,0 +1,65 @@
+import {spawnSync} from 'node:child_process';
+import fileSystem from 'node:fs';
+import {dirname} from 'node:path';
+import {argv} from 'node:process';
+
+if (argv.length === 3) {
+  buildPackage(argv[2]);
+} else {
+  console.error('Invalid command. Usage: {tag}');
+}
+
+function buildPackage(version) {
+  removeDirectory('./package/');
+  buildTypeScript([
+    './project/tsconfig.cjs.json',
+    './project/tsconfig.esm.json',
+    './project/tsconfig.types.json',
+  ]);
+  saveJsonFile('./package/package.json',
+    distributionPackageJson(
+      './public/package.json',
+      '../package.json',
+      version));
+  saveFile('./package/LICENSE', readFile('./public/LICENSE'));
+}
+
+function distributionPackageJson(templatePackage, sourcePackage, version) {
+  return {
+    ...readJsonFile(templatePackage),
+    version,
+    dependencies: readJsonFile(sourcePackage).dependencies,
+  };
+}
+
+function removeDirectory(path) {
+  fileSystem.rmSync(path, {recursive: true, force: true});
+}
+
+function buildTypeScript(projects) {
+  spawnSync('yarn', ['run', 'tsc', '--build', ...projects], {stdio: 'inherit', shell: true});
+}
+
+function readJsonFile(path) {
+  return JSON.parse(readFile(path));
+}
+
+function readFile(path) {
+  return fileSystem.readFileSync(path).toString();
+}
+
+function saveJsonFile(path, content) {
+  saveFile(path, JSON.stringify(content, null, 2));
+}
+
+function saveFile(path, content) {
+  createParentDirectory(path);
+  fileSystem.writeFileSync(path, content);
+}
+
+function createParentDirectory(path) {
+  const parent = dirname(path);
+  if (!fileSystem.existsSync(parent)) {
+    fileSystem.mkdirSync(parent);
+  }
+}
